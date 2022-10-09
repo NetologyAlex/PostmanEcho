7ed4726060bf7c7af293b2fded496998779244ca
NetologyAlex <96003016+NetologyAlex@users.noreply.github.com>
2022-10-10 00:31:08 +0300

    Update postman.java

--- src/test/java/postman.java
+++ src/test/java/postman.java
@@ -7,21 +7,16 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class postman {
 
-    @Test
-    void shouldReturnValue() {
-
-        // Given - When - Then
-        // Предусловия
-        given()
-
-                .baseUri("https://postman-echo.com")
-                .contentType("text/plain; charset=UTF-8")
-                .body("some data")
-                .when()
-                .post("/post")
-                .then()
-                .statusCode(200)
-                .body("data", equalTo("some value"))
-        ;
-    }
+  @Test
+     void shouldShowBody(){
+    given()
+        .baseUri("https://postman-echo.com")
+        .body("some data")
+        .when()
+        .post("/post")
+        .then()
+        .statusCode(200)
+        .body("data", equalTo("some data"))
+   ;
+  }
 }
